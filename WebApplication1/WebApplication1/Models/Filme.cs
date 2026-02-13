1d386b84aee8a25d0381718c3e0c21f7dd7e901a
nmedina <nicolascostam23@gmail.com>
2026-02-13 13:48:52 -0300

    :sparkles: feat: Add filme domain model

---
+++ WebApplication1/WebApplication1/Models/Filme.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Models;
+
+public class Filme
+{
+    public string Title { get; set; }
+    public string Genero { get; set; }
+    public int Duracao { get; set; }
+    public string Diretor { get; set; }
+}
