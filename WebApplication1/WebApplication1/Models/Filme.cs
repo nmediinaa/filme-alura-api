9bc1caa40798035e755136699545f95f9b524f1e
nmedina <nicolascostam23@gmail.com>
2026-02-13 14:10:55 -0300

    :sparkles: feat: Add data annotations to Filme model

--- WebApplication1/WebApplication1/Models/Filme.cs
+++ WebApplication1/WebApplication1/Models/Filme.cs
@@ -1,9 +1,19 @@
-﻿namespace WebApplication1.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models;
 
 public class Filme
 {
+    [Required(ErrorMessage = "O titulo do filme é obrigatório")]
     public string Title { get; set; }
+    
+    [Required(ErrorMessage = "O Genero do filme é obrigatório")]
     public string Genero { get; set; }
+    
+    [Required]
+    [Range(70, 600, ErrorMessage = "A duraçao do filme deve ter entre 70 e 600 minutos")]
     public int Duracao { get; set; }
+    
+    [Required(ErrorMessage = "O Diretor do filme é obrigatório")]
     public string Diretor { get; set; }
 }
