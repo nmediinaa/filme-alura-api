da6686abe8a86f9d1dc71922a2a09a6a34fbf9d2
nmedina <nicolascostam23@gmail.com>
2026-02-13 13:50:20 -0300

    :sparkles: feat: FilmeController endpoint

---
+++ WebApplication1/WebApplication1/Controllers/FilmeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+
+    private static List<Filme> _filmesList = new List<Filme>();
+    
+    [HttpPost]
+    public void AdicionaFilme([FromBody] Filme filme)
+    {
+        _filmesList.Add(filme);
+        Console.WriteLine(filme.Title);
+        Console.WriteLine(filme.Diretor);
+    }
+    
+    
+    
+}
