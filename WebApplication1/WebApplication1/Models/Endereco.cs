83f3c9738c54e96b2a0c042dcd7483a6a08b45e2
nmedina <nicolascostam23@gmail.com>
2026-03-05 18:00:00 -0300

    :sparkles: feat: Add a Endereco model class

---
+++ WebApplication1/WebApplication1/Models/Endereco.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models;
+
+public class Endereco
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+
+    [MaxLength(50, ErrorMessage = "Logradouro deve conter somente 50 caracteres")]
+    public string Logradouro { get; set; }
+
+    [Range(1, int.MaxValue, ErrorMessage = "O numero deve ser positivo")]
+    public int Numero{ get; set; }
+}
